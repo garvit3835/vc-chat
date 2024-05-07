c5f7086e3522a715ec8ae530aa10032e083f7f92
garvit1189 <97427673+garvit3835@users.noreply.github.com>
2024-05-07 19:38:28 +0530

    alert

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ app.get("/", async (req: Request, res: Response) => {
   // Call the CPU-intensive task
   const primes = calculatePrimes();
 
-  res.status(200).send("Hello World! yesss");
+  res.status(200).send("Hello World! yesss okay?");
 });
 
 
