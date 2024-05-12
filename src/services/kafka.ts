a31478af9a30cc0e7d170a1720928508e51fae66
garvit1189 <97427673+garvit3835@users.noreply.github.com>
2024-05-12 11:03:57 +0530

    sonar

--- src/services/kafka.ts
+++ src/services/kafka.ts
@@ -37,34 +37,7 @@ export async function produceMessage(message: string) {
 }
 
 export async function startMessageConsumer() {
-  console.log("Consumer is running..");
-  const consumer = kafka.consumer({ groupId: "default" });
-  await consumer.connect();
-  await consumer.subscribe({ topic: "MESSAGES", fromBeginning: true });
-
-  await consumer.run({
-    autoCommit: true,
-    eachMessage: async ({ message, pause }) => {
-      if (!message.value) return;
-      console.log(`New Message Recv..`);
-      try {
-        await prismaClient.message.create({
-          data: {
-            text: message.value?.toString(),
-          },
-        });
-      } catch (err) {
-        console.log("Something is wrong");
-        pause();
-        setTimeout(() => {
-          consumer.resume([{ topic: "MESSAGES" }]);
-        }, 60 * 1000);
-      }
-    },
-  });
-}
-export async function startMessageConsumer() {
-  console.log("Consumer is running..");
+  console.log("Consumer is running...");
   const consumer = kafka.consumer({ groupId: "default" });
   await consumer.connect();
   await consumer.subscribe({ topic: "MESSAGES", fromBeginning: true });
